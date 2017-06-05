2bfa057a34a4808315851ea0bc3e95e66d1b4d79
truongle <truonglvx@gmail.com>
2017-06-05 16:59:59 +0700

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-  devise_for :users
-  resources :tests
-  resources :posts
-  resources :users, only: [:index, :show, :edit, :update]
+  get 'static_pages/about'
   
-  root "application#hello"
+
+  root "static_pages#home"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
